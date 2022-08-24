e6bd95be5ffad673e9be0cb242367ed4d509852a
Santiago Zapata <72705458+santiago2588@users.noreply.github.com>
2022-08-24 16:39:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ df=pd.read_csv('emission factors.csv')
 
 fuels=df['fuel_name']
 
-fuel_selection=st.select('Fuel',fuels)
+fuel_selection=st.multiselect('Fuel',fuels)
 
 # Mask to filter dataframe
 mask_fuels = df['fuel_name'].isin(fuel_selection)
