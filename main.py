becee9c183460dff5cb992d6026f2599f13a1bde
Santiago Zapata <72705458+santiago2588@users.noreply.github.com>
2022-08-24 16:42:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -23,3 +23,6 @@ mask_fuels = df['fuel_name'].isin(fuel_selection)
 df = df[mask_fuels]
 df.drop(['heat_content','unit_heat_content'],axis=1,inplace=True)
 st.write(df)
+
+for fuel in fuel_selection:
+  fuel_consumption=st.number_input("Enter fuel consumption",min_value=1)
