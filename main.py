c5c918c049a291166fe400f8c16a82a786542607
Santiago Zapata <72705458+santiago2588@users.noreply.github.com>
2022-08-24 20:58:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ fuel_list=st.multiselect('Fuel',fuels)
 # Filtrar dataframe 
 mask_fuels = df['fuel_name'].isin(fuel_list)
 df = df[mask_fuels]
-df.drop(['scope','fuel_type','heat_content','unit_heat_content'],axis=1,inplace=True)
+#df.drop(['scope','fuel_type','heat_content','unit_heat_content'],axis=1,inplace=True)
 st.write(df)
 
 #Obtener listado de consumos
