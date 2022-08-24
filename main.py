baf36fd922fbcf6424c1c4641fa7b02b63ace07c
Santiago Zapata <72705458+santiago2588@users.noreply.github.com>
2022-08-24 18:20:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -28,11 +28,12 @@ st.write(df)
 df0=[]
 
 
-fuel_selection=st.selectbox('Fuel',fuels)
-fuel_consumption=st.number_input("Enter fuel consumption",min_value=1)
+#fuel_selection=st.selectbox('Fuel',fuels)
+for i in fuels:
+    fuel_consumption=st.number_input("Enter fuel consumption",min_value=1)
 
 if st.button("Add row"):
-    df0.append({"Fuel": fuel_selection, "consumption": fuel_consumption})
+    df0.append({"Fuel": fuels, "consumption": fuel_consumption})
     st.write(pd.DataFrame(df0))
 
  
