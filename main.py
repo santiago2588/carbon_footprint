044b24561a45e8f128e37607932d974c0d31f5f6
Santiago Zapata <72705458+santiago2588@users.noreply.github.com>
2022-08-24 15:28:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,40 +6,15 @@ Created on Sun Aug 14 18:19:54 2022
 """
 
 import streamlit as st
-import base64
 import numpy as np
 import pandas as pd
 
-st.title(' ')
-def get_base64(bin_file):
-    with open(bin_file, 'rb') as f:
-       data = f.read()
-    return base64.b64encode(data).decode()
-
-def set_background(png_file):
-    bin_str = get_base64(png_file)
-    page_bg_img = '''
-        <style>
-    .stApp {
-    background-image: url("data:image/png;base64,%s");
-    background-size: cover;
-    }
-    </style>
-        ''' % bin_str
-    st.markdown(page_bg_img, unsafe_allow_html=True)
-
-set_background('background.png')
-
-uploaded_file = st.file_uploader("Choose a XLSX file", type="xlsx")
-if uploaded_file:
-    df = pd.read_excel(uploaded_file)
-
-    st.dataframe(df)
-    st.table(df)
-
-
 st.title("Calculo de la huella de carbono industrial")
 
-#st.multiselect('Seleccione los combustibles',data)
+df = pd.read_excel("emission factors.xlsx")
+
+fuel_names=df['fuel_name']
+
+st.multiselect('Seleccione los combustibles',fuel_names)
 
 
