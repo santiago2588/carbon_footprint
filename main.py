ac651159cd30eae5b8a919166263a505c7872d1c
Santiago Zapata <72705458+santiago2588@users.noreply.github.com>
2022-08-24 18:00:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -31,11 +31,10 @@ fuel_consumption=st.number_input("Enter fuel consumption",min_value=1)
 
 df0=[]
 
-for fuel in fuel_selection:
-    if st.button("Add row"):
-        df0.append({"Fuel": fuel_selection, "consumption": fuel_consumption})
+if st.button("Add row"):
+    df0.append({"Fuel": fuel_selection, "consumption": fuel_consumption})
     
-    st.write(pd.DataFrame(df0))
+st.write(pd.DataFrame(df0))
 
  
 #if st.button('Delete'):
