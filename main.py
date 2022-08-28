5d530367108014929df7c5236888789104bc6e9b
Santiago Zapata <72705458+santiago2588@users.noreply.github.com>
2022-08-28 18:11:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -80,87 +80,87 @@ with st.expander('1. Selecciona las fuentes de energia que utilizas en tu planta
     emissions_total=np.sum(results['CO2 emissions'])
     cost_total=np.sum(results['Fuel cost USD'])
 
-st.subheader('3. Presiona el boton Resultados para obtener las emisiones de carbono de tu planta y los costos de energia')
+with st.expander('2. Presiona el boton Resultados para obtener las emisiones de carbono de tu planta y los costos de energia'):
 
-if st.button("Resultados"):
-    st.write(results)
-    
-    st.success('Resultados de tus emisiones de carbono')
-    
-    st.metric('Total emisiones',str("%.1f" % np.float_(emissions_total))+ ' CO2-eq')
-    
-    col1,col2,col3=st.columns(3)
+    if st.button("Resultados"):
+        st.write(results)
+
+        st.success('Resultados de tus emisiones de carbono')
+
+        st.metric('Total emisiones',str("%.1f" % np.float_(emissions_total))+ ' CO2-eq')
+
+        col1,col2,col3=st.columns(3)
+
+        with col1:
+            st.metric('Emisiones Alcance 1-Combustion fija',str("%.1f" % np.float_(emissions_scope1_fija))+' CO2-eq')
+
+        with col2:
+            st.metric('Emisiones Alcance 1-Combustion movil',str("%.1f" % np.float_(emissions_scope1_movil))+' CO2-eq')
+
+        with col3:
+            st.metric('Emisiones Alcance 2',str("%.1f" % np.float_(emissions_scope2))+' CO2-eq')
+
+        st.success('Resultados de tus costos energeticos')
+
+        st.metric('Costo total de energia',str("%.1f" % np.float_(cost_total))+ ' USD')
         
-    with col1:
-        st.metric('Emisiones Alcance 1-Combustion fija',str("%.1f" % np.float_(emissions_scope1_fija))+' CO2-eq')
-            
-    with col2:
-        st.metric('Emisiones Alcance 1-Combustion movil',str("%.1f" % np.float_(emissions_scope1_movil))+' CO2-eq')
-            
-    with col3:
-        st.metric('Emisiones Alcance 2',str("%.1f" % np.float_(emissions_scope2))+' CO2-eq')
+
+with st.expander("3. Presiona el boton Optimizar para descubrir cuanto podrias disminuir tus emisiones de carbono y tus costos de energia con nuestra tecnologia"):      
+
+    if st.button('Optimizar'): 
+
+        co2_reduced=emissions_total*0.2
+        co2_new=emissions_total*0.8
+
+        col1,col2=st.columns(2)
+
+        with col1:
+            st.metric('Emisiones que se pueden reducir',str("%.1f" % np.float_(co2_reduced))+ ' CO2-eq')
+
+        with col2:
+            st.metric('Tus nuevas emisiones de carbono son',str("%.1f" % np.float_(co2_new))+ ' CO2-eq')
+
+        st.success('Felicitaciones, has reducido tus emisiones de carbono y ahora tu planta es mas sustentable!')
+
+        cost_reduced=cost_total*0.2
+        cost_new=cost_total*0.8
+
+        col1,col2=st.columns(2)
+
+        with col1:
+            st.metric('Costos de energia que se pueden reducir',str("%.1f" % np.float_(cost_reduced))+ ' USD')
+
+        with col2:
+            st.metric('Tus nuevos costos de energia son',str("%.1f" % np.float_(cost_new))+ ' USD')
+
+        st.success('Felicitaciones, has reducido tus costos energeticos y ahora tu planta es mas rentable y eficiente!')
+    
+with st.expander("4. Presiona el boton Beneficios para descubrir como podemos ayudarte a conseguir mas beneficios con nuestro producto")
         
-    st.success('Resultados de tus costos energeticos')
-        
-    st.metric('Costo total de energia',str("%.1f" % np.float_(cost_total))+ ' USD')
-        
+    if st.button('Beneficios'): 
+            st.success("Felicitaciones, has comenzado a desbloquear algunos de los beneficios de la digitalización para el calculo de la huella de carbono en tu empresa, y has demostrado que tu planta puede ser mas sustentable y a la vez reducir tus costos operativos") 
+            st.markdown("En esta ocasion, te hemos presentado los beneficios del modulo de Calculos. Sin embargo, quedan inmensas oportunidades para disminuir tu huella de carbono al integrar todos los modulos de AUCAF") 
+            st.markdown("Si estás listo para emprender este camino y llevar la digitalización al siguiente nivel, contactate con nosotros para guiarte en el proceso y recibir asesoria de nuestros expertos para optimizar tus operaciones.")
 
-st.subheader("4. Presiona el boton Optimizar para descubrir cuanto podrias disminuir tus emisiones de carbono y tus costos de energia con nuestra tecnologia")      
+            contact_form = """
+            <form action="https://formsubmit.co/zapaz.consultores@gmail.com" method="POST">
+                 <input type="hidden" name="_captcha" value="false">
+                 <input type="text" name="Nombre" placeholder="Tu nombre" required>
+                 <input type="email" name="email" placeholder="Tu email" required>
+                 <textarea name="message" placeholder="Tu mensaje"></textarea>
+                 <button type="submit">Send</button>
+            </form>
+            """
 
-if st.button('Optimizar'): 
-    
-    co2_reduced=emissions_total*0.2
-    co2_new=emissions_total*0.8
-    
-    col1,col2=st.columns(2)
-    
-    with col1:
-        st.metric('Emisiones que se pueden reducir',str("%.1f" % np.float_(co2_reduced))+ ' CO2-eq')
-    
-    with col2:
-        st.metric('Tus nuevas emisiones de carbono son',str("%.1f" % np.float_(co2_new))+ ' CO2-eq')
-    
-    st.success('Felicitaciones, has reducido tus emisiones de carbono y ahora tu planta es mas sustentable!')
-        
-    cost_reduced=cost_total*0.2
-    cost_new=cost_total*0.8
-    
-    col1,col2=st.columns(2)
-    
-    with col1:
-        st.metric('Costos de energia que se pueden reducir',str("%.1f" % np.float_(cost_reduced))+ ' USD')
-    
-    with col2:
-        st.metric('Tus nuevos costos de energia son',str("%.1f" % np.float_(cost_new))+ ' USD')
+            st.markdown(contact_form, unsafe_allow_html=True)
 
-    st.success('Felicitaciones, has reducido tus costos energeticos y ahora tu planta es mas rentable y eficiente!')
-    
-st.subheader("5. Presiona el boton Beneficios para descubrir como podemos ayudarte a conseguir mas beneficios con nuestro producto")
-        
-if st.button('Beneficios'): 
-        st.success("Felicitaciones, has comenzado a desbloquear algunos de los beneficios de la digitalización para el calculo de la huella de carbono en tu empresa, y has demostrado que tu planta puede ser mas sustentable y a la vez reducir tus costos operativos") 
-        st.markdown("En esta ocasion, te hemos presentado los beneficios del modulo de Calculos. Sin embargo, quedan inmensas oportunidades para disminuir tu huella de carbono al integrar todos los modulos de AUCAF") 
-        st.markdown("Si estás listo para emprender este camino y llevar la digitalización al siguiente nivel, contactate con nosotros para guiarte en el proceso y recibir asesoria de nuestros expertos para optimizar tus operaciones.")
-    
-        contact_form = """
-        <form action="https://formsubmit.co/zapaz.consultores@gmail.com" method="POST">
-             <input type="hidden" name="_captcha" value="false">
-             <input type="text" name="Nombre" placeholder="Tu nombre" required>
-             <input type="email" name="email" placeholder="Tu email" required>
-             <textarea name="message" placeholder="Tu mensaje"></textarea>
-             <button type="submit">Send</button>
-        </form>
-        """
-
-        st.markdown(contact_form, unsafe_allow_html=True)
-
-        # Use Local CSS File
-        def local_css(file_name):
-            with open(file_name) as f:
-                st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+            # Use Local CSS File
+            def local_css(file_name):
+                with open(file_name) as f:
+                    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 
-        local_css("style.css")
+            local_css("style.css")
 
 
 
