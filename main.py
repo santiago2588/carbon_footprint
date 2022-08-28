c93a49a03e9907bbc2391b4a217d4df5180169fd
Santiago Zapata <72705458+santiago2588@users.noreply.github.com>
2022-08-28 18:40:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -43,14 +43,16 @@ with st.expander('Qué es la huella de carbono y cómo se calcula?'):
                 (por ejemplo, la gasolina o fuel oil) y al consumo de electricidad en las operaciones industriales.")
     
     st.markdown("De acuerdo con estándares internacionales, se distinguen dos tipos de alcances para el cálculo de la huella de carbono. El primer alcance se relaciona con las\
-                 emisiones por la combustión de combustibles en fuentes fijas (por ejemplo, calderos) o móviles (por ejemplo, vehículos. El segundo alcance se relaciona\
-                 con las emisiones por el consumo de electricidad en las operaciones (por ejemplo, para operar un equipo industrial")
+                 emisiones por la combustión de combustibles en fuentes fijas (por ejemplo, calderos) o móviles (por ejemplo, vehículos). El segundo alcance se relaciona\
+                 con las emisiones por el consumo de electricidad en las operaciones (por ejemplo, para operar un equipo industrial).")
     
     #image1 = Image.open('modulos ASTRO.png')
     #st.image(image1)
     
 
 with st.expander('Instrucciones'):
+    
+    st.markdown("AUCAF es una herramienta digital que permite calcular la huella de carbono en las operaciones industriales, y los costos de energía asociados")
     
     st.markdown("AUCAF es una solucion modular que se adapta a tus necesidades y genera valor en tus operaciones. AUCAF incluye los siguientes modulos:")
     
