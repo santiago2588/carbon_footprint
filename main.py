3d4be0f34ff4ddcb361903f13118feb67fecc63d
Santiago Zapata <72705458+santiago2588@users.noreply.github.com>
2022-08-29 22:22:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -42,9 +42,13 @@ with st.expander('Qué es la huella de carbono y cómo se calcula?'):
     st.markdown("La huella de carbono representa las emisiones de los gases de efecto invernadero debido al consumo de combustibles de origen fósil,\
                 (por ejemplo, la gasolina o fuel oil) y al consumo de electricidad en las operaciones industriales.")
     
-    st.markdown("De acuerdo con estándares internacionales, se distinguen dos tipos de alcances para el cálculo de la huella de carbono. El primer alcance se relaciona con las\
-                 emisiones por la combustión de combustibles en fuentes fijas (por ejemplo, calderos) o móviles (por ejemplo, vehículos). El segundo alcance se relaciona\
-                 con las emisiones por el consumo de electricidad en las operaciones (por ejemplo, para operar un equipo industrial).")
+    st.markdown(
+        """
+        De acuerdo con estándares internacionales, se distinguen dos tipos de alcances para el cálculo de la huella de carbono. 
+        - El primer alcance se relaciona con las emisiones por la combustión de combustibles en fuentes fijas (por ejemplo, calderos) o móviles (por ejemplo, vehículos). 
+        - El segundo alcance se relaciona con las emisiones por el consumo de electricidad en las operaciones (por ejemplo, para operar un equipo industrial).
+                 """
+                )
     
     #image1 = Image.open('modulos ASTRO.png')
     #st.image(image1)
@@ -52,7 +56,8 @@ with st.expander('Qué es la huella de carbono y cómo se calcula?'):
 
 with st.expander('Instrucciones'):
     
-    st.markdown("###AUCAF es una herramienta digital que permite calcular la huella de carbono en las operaciones industriales, y los costos de energía asociados")
+    st.markdown("""
+    ###AUCAF es una herramienta digital que permite calcular la huella de carbono en las operaciones industriales, y los costos de energía asociados""")
     
     st.markdown("AUCAF es una solucion modular que se adapta a tus necesidades y genera valor en tus operaciones. AUCAF incluye los siguientes modulos:")
     
