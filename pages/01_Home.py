18ca24df58af9255a6444737c89d15067c8f077b
Santiago Zapata <72705458+santiago2588@users.noreply.github.com>
2022-08-31 21:53:37 +0100

    Update and rename 01_Home.py to 01_Instrucciones.py

--- pages/01_Home.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.set_page_config(
-        page_title="My Page Title",
-)
-st.markdown("# Page 2 ❄️")
-st.sidebar.markdown("# Page 2 ❄️")
-
-st.write("## Bienvenido al demo de AUCAF, la herramienta digital para el calculo y reduccion de la huella de carbono industrial 👋") 
