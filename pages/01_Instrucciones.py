c286b44e96442257acc7b06797883d309040a285
Santiago Zapata <72705458+santiago2588@users.noreply.github.com>
2022-08-31 21:54:02 +0100

    Update 01_Instrucciones.py

--- pages/01_Instrucciones.py
+++ pages/01_Instrucciones.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.markdown("# Instrucciones"
+st.markdown("# Instrucciones")
 st.sidebar.markdown("# Instrucciones")
 
 st.write("## Bienvenido al demo de AUCAF, la herramienta digital para el calculo y reduccion de la huella de carbono industrial") 
