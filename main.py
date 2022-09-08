22f670104218f5561d974bb0fc5893e11ceeac2e
Santiago <santiago.zapata2588@gmail.com>
2022-09-08 20:23:19 +0100

    Added text

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import streamlit as st
 st.set_page_config(layout="wide",page_title="AUCAF",page_icon="👋")
 
 from PIL import Image
-image = Image.open('logo_Pungo.png') 
+image = Image.open('Resources/logo_Pungo.png')
 st.image(image)
 
 st.write("# Bienvenido al demo de AUCAF: la herramienta digital para el cálculo, monitoreo y reducción de la huella de carbono en la industria")
