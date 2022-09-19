8d18e4cbe868c7bc45c0040d72b6be03ae798d68
Santiago <santiago.zapata2588@gmail.com>
2022-09-19 20:30:38 +0100

    Added text

--- pages/02_Calculos.py
+++ pages/02_Calculos.py
@@ -43,7 +43,7 @@ with st.expander('1. Selecciona las fuentes de energia que utilizas en tu planta
     #Obtener listado de consumos
     for i in fuel_list:
         fuel_unit=df.query("fuel_name==@i")['consumption_unit'].to_string(index=False)
-        consumption=st.number_input("Consumo "+ str(i)+" "+fuel_unit,key=i)
+        consumption=st.number_input("Consumo "+ str(i)+"en "+fuel_unit,key=i)
         consumption_list.append(consumption)
 
     #Prueba de la funcion
