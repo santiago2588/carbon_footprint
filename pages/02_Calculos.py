6a56819d66b91da448db5b7876d3624d1ae5f351
Santiago <santiago.zapata2588@gmail.com>
2022-10-24 12:55:26 +0100

    Added text

--- pages/02_Calculos.py
+++ pages/02_Calculos.py
@@ -83,7 +83,7 @@ with st.expander('1. Selecciona las fuentes de energia que utilizas en tu planta
 
     fuel_name=pd.DataFrame(df0).transpose().reset_index(drop=True)
     fuel_name.columns=['Fuente energia']
-    consumption_value=pd.DataFrame(consumption_list).transpose().reset_index(drop=True)
+    consumption_value=pd.DataFrame(consumption_list).reset_index(drop=True)
     consumption_value.columns=['Consumo energia']
     co2=pd.DataFrame(df1).transpose().reset_index(drop=True)
     co2.columns=['Emisiones kg CO2-eq']
