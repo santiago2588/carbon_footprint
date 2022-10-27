43ee8941d686b6ccecbd2fcc562dcee7fb51195e
Santiago <santiago.zapata2588@gmail.com>
2022-10-27 22:02:04 +0100

    Added text

--- pages/02_Calculos.py
+++ pages/02_Calculos.py
@@ -48,7 +48,8 @@ with st.expander('1. Selecciona las fuentes de energia que utilizas en tu planta
     industry_ref=st.checkbox("Usar fuentes de energía más utilizadas en la industria")
 
     if industry_ref:
-        fuel_list=['ELECTRICIDAD SISTEMA NACIONAL INTERCONECTADO','FUEL OIL LIVIANO','GAS LICUADO DE PETROLEO (GLP) INDUSTRIAL'].sort()
+        fuel_list=['ELECTRICIDAD SISTEMA NACIONAL INTERCONECTADO','FUEL OIL LIVIANO','GAS LICUADO DE PETROLEO (GLP) INDUSTRIAL']
+        fuel_list.sort()
         #consumption_ref={'GAS LICUADO DE PETROLEO (GLP) INDUSTRIAL':10,'FUEL OIL LIVIANO':100,'ELECTRICIDAD SISTEMA NACIONAL INTERCONECTADO':1000}
 
     # Filtrar dataframe
