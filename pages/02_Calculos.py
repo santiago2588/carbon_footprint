78ee84d8833716c10bf51d839dda5d84f5def7c1
Santiago <santiago.zapata2588@gmail.com>
2022-10-27 21:54:18 +0100

    Added text

--- pages/02_Calculos.py
+++ pages/02_Calculos.py
@@ -41,7 +41,8 @@ with st.expander('1. Selecciona las fuentes de energia que utilizas en tu planta
     #Obtener listado de combustibles
     fuels=df['fuel_name']
 
-    fuel_list=st.multiselect('Fuentes de energia',fuels).sort()
+    fuel_list=st.multiselect('Fuentes de energia',fuels)
+    fuel_list.sort()
 
     container=st.beta_container
     industry_ref=st.checkbox("Usar fuentes de energía más utilizadas en la industria")
