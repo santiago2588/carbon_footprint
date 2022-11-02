6f53a73ecbeaebe796831e0ab6bab1159a6e0b13
Santiago <santiago.zapata2588@gmail.com>
2022-11-02 11:07:38 +0000

    Added text

--- pages/02_Calculos.py
+++ pages/02_Calculos.py
@@ -127,7 +127,7 @@ with st.expander('2. Calcula las emisiones de carbono de tu planta y los costos 
 
     st.dataframe(results)
 
-    fig_results = px.treemap(results, names='Fuente energia', values='Emisiones kg CO2-eq',hover_data=('Costo energia USD','Alcance emisiones'),title='Contribución fuentes de energía')
+    fig_results = px.treemap(results, names='Fuente energia', values='Emisiones kg CO2-eq',parents='Alcance emisiones',hover_data=('Costo energia USD','Alcance emisiones'),title='Contribución fuentes de energía')
     #fig_results.update_traces()
     st.plotly_chart(fig_results, use_container_width=True)
 
