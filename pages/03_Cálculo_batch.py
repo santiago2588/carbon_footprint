a8c46bfc362e3a8f013ec3bfd41f642795f20521
Santiago <santiago.zapata2588@gmail.com>
2022-11-09 22:02:53 +0000

    Added text

--- pages/03_Cálculo_batch.py
+++ pages/03_Cálculo_batch.py
@@ -91,13 +91,13 @@ if file_upload is not None:
         results=pd.concat([process_name,equipment_name,fuel_name,emission_factor,fuel_cost,co2,scope,cost],axis='columns')
         #results.set_index('ID proceso',inplace=True)
 
-        emissions_total=np.sum(results['Emisiones kg CO2-eq'])
-        cost_total=np.sum(results['Costo energia USD'])
+        #emissions_total=np.sum(results['Emisiones kg CO2-eq'])
+        #cost_total=np.sum(results['Costo energia USD'])
 
         col1,col2=st.columns(2)
 
         with col1:
-            st.metric('Total emisiones de carbono',str("%.1f" % emissions_total)+ ' kg CO2-eq')
+            st.metric('Total emisiones de carbono',str("%.1f" % emissions_total))+ ' kg CO2-eq')
 
         with col2:
             st.metric('Costo total de energia',str("%.1f" % np.float_(cost_total))+ ' USD')
